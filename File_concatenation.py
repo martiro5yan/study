268db01f2debdd2d6e86f468437c080c6acc0619
Arsen Martirosyan <152820589+martiro5yan@users.noreply.github.com>
2024-01-19 17:06:45 +0300

    Day 55 / File operations

---
+++ File_concatenation.py
@@ -0,0 +1,16 @@
+def File_concatenation(listFile):
+    output = []
+    def Gluing(fileName):
+        with open(fileName, 'r', encoding='utf-8') as file:
+            content = file.readlines()
+            for line in content:
+                output.append(line)
+    def newOutput(line):
+        with open('output.txt','a', encoding='utf-8') as file:
+            file.write(line)
+    for fileName in listFile:
+        Gluing(fileName)
+
+    for line in output:
+        newOutput(line)
+    print('The file output.txt assembled!')
